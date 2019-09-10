96588e578b6f89998eab35aff43413c4fef366ec
Max Goad <maxdgoad@gmail.com>
2019-09-10 13:52:22 -0500

    fixed some lines but still not compiling

--- BattleShip.java
+++ BattleShip.java
@@ -2,7 +2,8 @@
 package BattleShip;
 
 import javafx.application.Application;
-import javafx.event.ActionHandler;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
