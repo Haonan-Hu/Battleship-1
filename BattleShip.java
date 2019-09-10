76f165add1e21b0b05fa57cacfedce496fd6dcce
Meet Kapadia <mkapadia70@ku.edu>
2019-09-10 14:01:04 -0500

    early stages of the "main" file (please see if I am on the right track!)

--- BattleShip.java
+++ BattleShip.java
@@ -1,7 +1,14 @@
 public class BattleShip{
-
+    public static int numRows = 8;
+    public static int numCols = 8;
+    public static int player1Ships;
+    public static int player2Ships;
+    public static String[][] grid = new String[numRows][numCols];
+    public static int[][] missedGuesses = new int[numRows][numCols];
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        System.out.println("Welcome to Battleship!!");
+        //Other "run" fuctions will go here!
+
     }
 }
