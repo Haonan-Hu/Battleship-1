8e8ff23dc3fc40b18a40ca5c5ce184ac3837ba50
Jace Bayless <jacebayless@gmail.com>
2019-09-15 14:56:01 -0500

    Added more functionality to fire, created a getBoard, and gameOver function.

--- GameBoard.java
+++ GameBoard.java
@@ -1,17 +1,18 @@
 public class GameBoard{
     private int boardSize;
     private int[][] board;
+    private Ship[] ships;
 
     /*** BOARD VARIABLES ***
         0 = EMPTY SPACE
         1 = SHIP
         2 = SHIP HIT
-        3 = SUNK SHIP
+        3 = SUNK SHIP ?
      ***                 ***/
 
-
-    public GameBoard(int boardSize){
+    public GameBoard(int boardSize, Ship[] ships){
         this.boardSize = boardSize;
+        this.ships = ships;
         board = new int[boardSize][boardSize];
 
         for(int row = 0; row < boardSize; row++){
@@ -31,15 +32,36 @@ public class GameBoard{
     }
 
     public String fire(int x, int y){
-        if(x >= boardSize || y >= boardSize)
+        if(x >= boardSize || y >= boardSize || x < 0 || y < 0)
             return "Error: out of board bounds";
 
         if(board[x][y] == 0){
             return "Miss";
         }
         else if(board[x][y] == 1){
+            for(Ship ship : ships){
+                if(ship.contains(x,y)){
+                    ship.hit(x,y);
+                    return "Hit";
+                }
+            }
             return "HIT!";
         }
         return "Error";
+    }
+
+    public int[][] getBoard(){
+        return board;
+    }
+
+    public boolean gameOver(){
+        boolean gameOver = true;
+        for(Ship ship :ships){
+            if(!ship.sunk()){
+                gameOver = false;
+                break;
+            }
+            return gameOver;
+        }
     }
 }
