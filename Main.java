60477c5aaf4618956b4930aeb82f1d82c0ad856a
Meet Kapadia <mkapadia70@gmail.com>
2019-09-22 16:39:38 -0500

    added comments/documentation

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 class Main{
 
+  /*
+  * @ pre none
+  *	@ param string of args
+  *	@ post runs program
+  * @ return none
+  */
     public static void main(String args[]){
         Game game = new Game();
     }
