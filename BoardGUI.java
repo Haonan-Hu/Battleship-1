25df28d1f3fb278d30f9087498c4f92d3034dc93
Max Goad <maxdgoad@gmail.com>
2019-09-21 22:24:09 -0500

    working on ship rotation and gui on the buttons

--- BoardGUI.java
+++ BoardGUI.java
@@ -45,6 +45,9 @@ import javafx.scene.ImageCursor;
 
 import javafx.scene.Node;
 
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
+
 
 
 //#megaclass!!!!!
@@ -60,14 +63,15 @@ public class BoardGUI implements OverScene, EventHandler<ActionEvent>{
     private Scene options;
     private int rows = 9, cols = 9;
     private int x, y; //x and y coord of button that is hovered over
-    private Image[] ships;
+    private Image[] ships, shipsInOrder;
     
     private int numOfShips;
     private GameBoard player1board, player2board;
     
-    private boolean p1selecting = false, p2selecting = false;
+    private boolean p1selecting = false, p2selecting = false, horizontal = true;
     
     private Button[][] board1, board2;
+    private int shipSelecting = 0;
     
     public BoardGUI(String gamemode, Stage s, Font f, int numOfShips){
      
@@ -77,9 +81,20 @@ public class BoardGUI implements OverScene, EventHandler<ActionEvent>{
         Image image = new Image(getClass().getResourceAsStream("images/water.png"));
         
         ships = new Image[5];
-       for(int rep = 0; rep< 5; rep++){
-           ships[rep] = new Image("images/1x" + Integer.toString(rep+1) + ".png", 50*(rep+1), 50, true, true);
-       }
+        shipsInOrder = new Image[5];
+        
+        for(int rep = 0; rep< 5; rep++){
+            ships[rep] = new Image("images/1x" + Integer.toString(rep+1) + ".png", 50*(rep+1), 50, true, true);
+        }
+        
+       
+        shipsInOrder[0] = new Image("images/front.png", 50, 50, true, true);
+        shipsInOrder[1] = new Image("images/mid.png", 50, 50, true, true);
+        shipsInOrder[2] = new Image("images/mid.png", 50, 50, true, true);
+        shipsInOrder[3] = new Image("images/mid.png", 50, 50, true, true);
+        shipsInOrder[4] = new Image("images/1x1.png", 50, 50, true, true);
+    
+        
         
         
         //Define their placement in the grid
@@ -202,7 +217,7 @@ public class BoardGUI implements OverScene, EventHandler<ActionEvent>{
         
         
         gr.setStyle("-fx-background-color: lightslategray;");
-        options = new Scene(gr, 1400, 800);
+        options = new Scene(gr, 1000, 800);
         
         
         //The highest level backend object.
@@ -214,8 +229,12 @@ public class BoardGUI implements OverScene, EventHandler<ActionEvent>{
         player1board = new GameBoard();
         player2board = new GameBoard();
         
-        
+        options.setCursor(new ImageCursor(ships[0],
+                                             ships[0].getWidth()/2,
+                                             ships[0].getHeight()/2));
         p1selecting = true;
+        
+        
       
         
     }
@@ -232,29 +251,18 @@ public class BoardGUI implements OverScene, EventHandler<ActionEvent>{
                                 ships[0].getWidth()/2,
                                 ships[0].getHeight()/2));*/
 
-               
-        
-               
-  
       
     }
     
-    public void placeShip(GameBoard player, int numShips, int shipCol, int shipRow){
-        for(int i = 1; i <= numShips; i++){
-            int shipLength = i;
+    public void placeShip(GameBoard player, int shipLength, int shipCol, int shipRow){
+        
             Ship tempShip = new Ship(shipLength);
+        
+ 
+            tempShip.addCoordinates(shipRow,shipCol);
 
-
-            for(int j = 1; j <= shipLength; j++){
-
-                if(tempShip.inline(shipRow, shipCol) && !player.isOccupied(shipRow, shipCol) && !tempShip.containsCoordinate(shipRow, shipCol))
-                    tempShip.addCoordinates(shipRow,shipCol);
-                else
-                    j--;
-            }   
 
             player.addShip(tempShip);
-        }
     }
 
     @Override
@@ -262,27 +270,80 @@ public class BoardGUI implements OverScene, EventHandler<ActionEvent>{
         return options;
     }
     
+    //keyboard event to switch ship orientation
+    
+    public void handle(KeyEvent k) {
+        
+        if(p1selecting || p2selecting){
+            if(k.getCode() == KeyCode.LEFT){
+                horizontal = !horizontal;
+                System.out.println("rotate");
+            
+            }
+        }
+            
+        
+        
+    }
+    
     @Override
 	public void handle(ActionEvent e) {
         
-        if(p1selecting){
+        if(p1selecting && shipSelecting < 5){
             //shipSelectionLoop();
+            
+            
             
             for(int x = 0; x <cols-1; x++){
                 for(int y = 0; y<rows-1; y++){
                     if(e.getSource() == board1[y][x] && !player1board.isOccupied(x, y)){
-                     
-                        //options.setCursor(Cursor.DEFAULT);
-                        placeShip(player1board, numOfShips, x, y);
-                        board1[y][x].setGraphic(new ImageView(ships[0]));
                         
+                        placeShip(player1board, shipSelecting+1, x, y);
+                        
+                        if(horizontal)
+                        {
+                        
+                            for(int rep = 0; rep<shipSelecting+1; rep++){
+
+                                board1[y][x+rep].setGraphic(new ImageView(shipsInOrder[rep]));
+
+                            }
+                        }
+                        
+                        else if(!horizontal)
+                        {
+                            for(int rep = 0; rep<shipSelecting+1; rep++){
+
+                                board1[y+rep][x].setGraphic(new ImageView(shipsInOrder[rep]));
+                                ((ImageView) board1[y+rep][x].getGraphic()).setRotate(180);
+
+                            }
+                            
+                        }
+                        
+                        
+                        //board1[y][x].setGraphic(new ImageView(ships[shipSelecting]));
+                        
+                        if(shipSelecting <5){
+                            shipSelecting++;
+                            options.setCursor(new ImageCursor(ships[shipSelecting],
+                                             ships[shipSelecting].getWidth()/2,
+                                             ships[shipSelecting].getHeight()/2));
+                        }
+                        
+                        
+                        
+                        
+
                     }
-                        
+                    
+                    else if(player1board.isOccupied(x,y)){
+                        //System.out.println("Invalid Spot");
+                    }        
                         
                 }
                 
             }
-            
             
         }
 
